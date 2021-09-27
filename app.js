2c812a0312c4eaa064aef48fe950de27f0e6dbcf
richie <richie.kwon1@gmail.com>
2021-09-27 21:25:18 +0100

    practice

---
+++ app.js
@@ -0,0 +1,7 @@
+function logname(name) {
+    console.log(name);
+}
+logname('Micela');
+console.log('hello world');
+var a = 5;
+a = 'hello';
---
+++ app.ts
@@ -0,0 +1,13 @@
+ function logname (name: string){
+     console.log(name);
+ }
+
+ logname('Micela');
+
+ console.log('hello world');
+ 
+function getStudent (ID: number):any
+{
+  return null; 
+}   
+  
